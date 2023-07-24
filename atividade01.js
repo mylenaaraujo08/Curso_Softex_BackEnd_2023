c042442a710959c1fdafad926d6699d6a6d2754b
mylenaaraujo08 <139914402+mylenaaraujo08@users.noreply.github.com>
2023-07-24 20:47:08 +0000

    first commit

---
+++ atividade01.js
@@ -0,0 +1,13 @@
+let minhavar = 10;
+let soma = 15 + 8;
+soma += 1;
+soma *= 3;
+console.log("a soma é", + soma);
+let fazsol = true;
+let comida = ["arroz", "feijão", "ovo"];
+console.log(comida[1]);
+if (minhavar <= soma) {
+  console.log("minhavar é menor ou igual a soma.");
+} else {
+  console.log("minhavar é maior que soma.");
+}
