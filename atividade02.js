91e34b33d31717c006210db48d87b3a5a96454b9
mylenaaraujo08 <139914402+mylenaaraujo08@users.noreply.github.com>
2023-07-26 10:52:44 -0300

    Add files via upload

---
+++ atividade02.js
@@ -0,0 +1,31 @@
+var numero = 42;
+if (numero > 0) {
+  console.log("O número é positivo.");
+} else if (numero === 0) {
+  console.log("O número é zero.");
+} else {
+  console.log("O número é negativo.");
+}
+if (numero % 2 === 0) {
+  console.log("O número é par.");
+} else {
+  console.log("O número é ímpar.");
+}
+var minhaString = "";
+
+if (minhaString.length === 0) {
+  console.log("A string está vazia.");
+} else {
+  console.log("A string não está vazia.");
+}
+if (numero > 10) {
+  console.log("O número é maior que 10.");
+} else {
+  console.log("O número não é maior que 10.");
+}
+var minhaVariavel = true;
+if (typeof minhaVariavel === "boolean") {
+  console.log("A variável é do tipo booleano.");
+} else {
+  console.log("A variável não é do tipo booleano.");
+}
