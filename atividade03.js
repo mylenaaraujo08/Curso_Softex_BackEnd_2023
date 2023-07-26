4ed9446678fb8cf35e367685b3bd8e28cc013511
mylenaaraujo08 <139914402+mylenaaraujo08@users.noreply.github.com>
2023-07-26 14:25:54 -0300

    Update atividade03.js

--- atividade03.js
+++ atividade03.js
@@ -1,4 +1,4 @@
-var podeDirigir(idade, temautorizacaoPais) {
+function podeDirigir(idade, temAutorizacaoPais) {
     if (idade >= 18) {
       return "Você tem 18 anos ou mais, pode dirigir sem a autorização dos pais.";
     } else if (idade >= 16 && temAutorizacaoPais) {
@@ -8,12 +8,12 @@ var podeDirigir(idade, temautorizacaoPais) {
     }
   }
   // Exemplos de uso:
-var idade1 = 15;
-var temAutorizacaoPais01 = false;
-console.log(podeDirigir(idade1, temAutorizacaoPais01)); 
-var idade2 = 16;
-var temAutorizacaoPais02 = true;
-console.log(podeDirigir(idade2, temAutorizacaoPais02)); 
-var idade3 = 18;
-var temAutorizacaoPais03 = false;
-console.log(podeDirigir(idade3, temAutorizacaoPais03));
+  var idade1 = 15;
+  var temAutorizacaoPais01 = false;
+  console.log (podeDirigir(idade1, temAutorizacaoPais01)); 
+  var idade2 = 16;
+  var temAutorizacaoPais02 = true;
+  console.log (podeDirigir(idade2, temAutorizacaoPais02)); 
+  var idade3 = 18;
+  var temAutorizacaoPais03 = false;
+  console.log (podeDirigir(idade3, temAutorizacaoPais03));
