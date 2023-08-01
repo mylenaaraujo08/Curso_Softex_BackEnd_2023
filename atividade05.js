c7b99eac5181a2ab24c05019aec421cf079aff21
mylenaaraujo08 <139914402+mylenaaraujo08@users.noreply.github.com>
2023-08-01 16:08:46 -0300

    Add files via upload

---
+++ atividade05.js
@@ -0,0 +1,15 @@
+const meuarray=[10,20,30,40,50];
+console.log(meuarray[2]);
+const frutas= [ "maçã", "banana", "laranja", "uva", "manga"];
+console.log(frutas. length);
+const pera = "pera";
+frutas.push(pera);
+console.log(frutas);
+frutas.splice (1,1);
+console.log(frutas);
+const numeros = [10, 20, 30, 40, 50];
+if (numeros.includes(25)) {
+  console.log("O número 25 foi encontrado no array.");
+} else {
+  console.log("O número 25 não foi encontrado no array.");
+}
