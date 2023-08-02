ed50cc40cf9f2593251c42fa0ec5a8f72d7727dd
mylenaaraujo08 <139914402+mylenaaraujo08@users.noreply.github.com>
2023-08-02 12:07:25 -0300

    Update atividade01.js
    1. Declarar uma variável chamada "minhavar", sem valor.
    2. Após declarada, atribua o valor 10 a variável "minhavar".
    3. Declare uma nova variável chamada "soma", e adicione uma instrução somando os valores 15 e 8.
    4. Atribua á variável "soma" todo o valor dela, somando 1, usando o operador de soma abreviado.
    5. Atribua á variável "soma" todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado,
    6. Qual é o valor da variável "soma" até aqui?
    7. Declare uma variável chamada "fazsol" atribuindo a ela o valor booleano que representa "verdadeiro"
    8. Declare uma variável chamada "comida" que recebe um array com valores "arroz", "feijão" e "ovo"
    9. Digite a instrução que imprime o valor de "feijão", que está na variável "comida".
    10. Digite o código que verifica se a variável "soma" é igual a variável "minhavar"  (testando também o tipo).
    11. Digite o código que verifica se a variável  "minhavar" é menor ou igual á variável "soma".
    12. Crie uma função chamada divisão que receba como parâmetro dois números, e retorne o resultado da divisão entre eles. 
    13. Invoque a função criada acima, passando os parâmetros 10 e 2.

--- atividade01.js
+++ atividade01.js
@@ -3,11 +3,26 @@ let soma = 15 + 8;
 soma += 1;
 soma *= 3;
 console.log("a soma é", soma);
+
 let fazsol = true;
 let comida = ["arroz", "feijão", "ovo"];
 console.log(comida[1]);
+
 if (minhavar <= soma) {
+  if (soma === minhavar) {
+    console.log("As variáveis são iguais, incluindo o tipo.");
+  } else {
+    console.log("As variáveis são diferentes ou têm tipos diferentes.");
+  }
   console.log("minhavar é menor ou igual a soma.");
 } else {
   console.log("minhavar é maior que soma.");
 }
+
+function divisao(numero1, numero2) {
+  if (numero2 === 0) {
+    return "Não é possível dividir por zero.";
+  }
+  return numero1 / numero2;
+}
+console.log(divisao(10, 2));
