712540de2799fcaeacf80debc9fbc0dc09e4147b
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2023-08-08 15:38:39 -0300

    Add files via upload

---
+++ atividade09.js
@@ -0,0 +1,6 @@
+const nome = "mylena";
+const idade = "24";
+const dia = "08";
+const mes = "agosto";
+const ano = "2023";
+console.log (` boa tarde, meu nome é ${nome}, tenho ${idade} anos, hoje é dia ${dia}, do mês de ${mes} , do ano de ${ano}`); 
