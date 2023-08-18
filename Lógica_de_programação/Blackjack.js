fd5ec9af4974c97f38f3f45dc04b507be53adc1d
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2023-08-18 11:36:21 -0300

    Create Blackjack.js
    No blackjack, o objetivo do jogo é ganhar o banco. Você vence se tiver uma pontuação maior do que o banco, mas menor que ou igual a 21
    1. Crie três variáveis: Duas cartas e soma delas.
    2. O seu código deve testar se a soma das suas duas cartas é maior que 21, caso verdadeiro, informe ao jogador que ele perdeu.
    3. Ao final do código, informe a soma dos pontos do jogador.
    Agora vamos adicionar um adversário ao jogo "o banco"
    4. Crie mais 5 variáveis sendo 4 cartas do banco e 1 com a soma delas.
    5. Crie uma nova lógica onde será testada se a soma das cartas do banco é maior que 21 ou se a soma das cartas é menor ou igual a 21 e suas cartas têm soma maior que o banco.
    6. Imprimir que o jogador venceu ou que o banco venceu, dependendo dos somatórios das cartas.

---
+++ Lógica_de_programação/Blackjack.js
@@ -0,0 +1,23 @@
+var cartas;
+var carta1 = 10;
+var carta2 = 8;
+var somacartas = carta1 + carta2;
+console.log("A soma das duas cartas é: " + somacartas);
+if (somacartas > 21) {
+  console.log("Você perdeu! A soma das cartas é maior que 21.");
+} else {
+  console.log("Você ganhou! A soma das cartas é menor ou igual a 21.");
+}
+console.log("Pontuação do jogador: " + somacartas); 
+var carta1 = 6;
+var carta2 = 4;
+var carta3 = 8;
+var carta4 = 8;
+var cartasoma = carta1 + carta2 + carta3 + carta4;
+console.log("A soma é", cartasoma);
+if (cartasoma > 21) {
+  console.log("Você perdeu! A soma das cartas é maior que 21.");
+} else {
+  console.log("Você ganhou! A soma das cartas é menor ou igual a 21.");
+}
+console.log("Pontuação do jogador: " + cartasoma);
