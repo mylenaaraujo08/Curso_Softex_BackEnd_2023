179d63a0cc4d3788f4e7da5b742884df440f27e2
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2023-08-18 12:37:35 -0300

    Delete atividade09.js

--- atividade09.js
+++
@@ -1,6 +0,0 @@
-const nome = "mylena";
-const idade = "24";
-const dia = "08";
-const mes = "agosto";
-const ano = "2023";
-console.log (` boa tarde, meu nome é ${nome}, tenho ${idade} anos, hoje é dia ${dia}, do mês de ${mes}, do ano de ${ano}`); 
