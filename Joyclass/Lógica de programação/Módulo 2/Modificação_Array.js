43888cb5068c7cc822fbb7a8226f3f47a993e3a0
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2023-08-19 14:05:55 -0300

    Add files via upload

---
+++ Joyclass/Lógica de programação/Módulo 2/Modificação_Array.js
@@ -0,0 +1,15 @@
+let nomes = ["Mylena", "Isabelle", "Jacqueline", "Mariana", "Grazielly", "Rayra", "Larissa", "Rafaela", "Iasmim", "Rayane"];
+let idades = [24, 19, 26, 26, 26, 24, 26, 26, 26, 26];
+let coresFavoritas = ["Amarelo", "Rosa", "Vermelho", "Roxo", "Azul", "laranja", "Beje", "preto", "branco", "marrom"];
+
+console.log("Nomes:", nomes);
+console.log("Idades:", idades);
+console.log("Cores Favoritas:", coresFavoritas);
+
+idades[1] = 20; 
+coresFavoritas[6] = "Verde";
+
+console.log("\nApós as modificações:");
+console.log("Nomes:", nomes);
+console.log("Idades:", idades);
+console.log("Cores Favoritas:", coresFavoritas);
