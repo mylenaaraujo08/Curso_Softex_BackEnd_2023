a9932183afc772a0634e46dfc366f22cebbee5e4
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2023-08-31 16:12:05 -0300

    Update Atividade Enumerates.js
    1. Suponha que você está desenvolvendo um aplicativo de agendamento e precisa representar os dias da semana. Como você usaria enums para isso e qual seria o benefício?
    2. Você está construindo um jogo e precisa representar os diferentes tipos de inimigos. Como você usaria enums para essa situação?
    3. Você está criando um aplicativo de reserva de passagens aéreas e precisa representar os diferentes tipos de classe (Econômica, Executiva, Primeira Classe). Como você usaria enums para isso?
    4. Imagine que você está desenvolvendo um aplicativo de tarefas e deseja categorizar as tarefas em "Trabalho", "Estudo" e "Lazer". Como você usaria enums para esse cenário?

--- Lógica_de_programação/Atividade Enumerates.js
+++ Lógica_de_programação/Atividade Enumerates.js
@@ -1,13 +1,13 @@
 const DiaDaSemana = {
   Domingo: "Domingo",
   Segunda: "Segunda-feira",
-  Terça: "terça-feira",
+  Terça: "Terça-feira",
   Quarta: "Quarta-feira",
   Quinta: "Quinta-feira",
   Sexta: "Sexta-feira",
   Sabado: "Sábado"
 };
-console.log (DiaDaSemana.Domingo);
+console.log(DiaDaSemana.Domingo);
 
 const TipoInimigo = {
   CASA1: "Stark",
@@ -23,9 +23,25 @@ let inimigo = {
 };
 
 if (inimigo.tipo === TipoInimigo.CASA2) {
-  console.log('Fortaleza da familia Targaryen!');
-} else if (inimigo.tipo === TipoInimigo.ORC) {
-  console.log('Fortaleza da familia Stark!');
+  console.log('Atacar fortaleza da familia Targaryen!');
+} else if (inimigo.tipo === TipoInimigo.CASA1) {
+  console.log('Atacar fortaleza da familia Stark!');
+}
+
+const reservaAeria = {
+  Economica: "Classe Econômica",
+  Executiva: "Executiva",
+  Primeira_Classe: "Primeira_Classe"
+};
+
+let reserva = {
+  Passagem: `Duas passagens`,
+  tipo: reservaAeria.Executiva
+};
+if (reserva.tipo === reservaAeria.Executiva) {
+  console.log(`Reservar passagem na Executiva`);
+} else if (reserva.tipo === reservaAeria.Primeira_Classe) {
+  console.log(`Reservar passagem na Primeira Classe`);
 }
 
 const CategoriaTarefa = {
@@ -35,8 +51,8 @@ const CategoriaTarefa = {
 };
 
 let tarefa = {
-  descricao: 'Fazer relatório mensal',
-  categoria: CategoriaTarefa.TRABALHO
+  descricao: 'Estudar para semana de prova',
+  categoria: CategoriaTarefa.ESTUDO
 };
 
 if (tarefa.categoria === CategoriaTarefa.ESTUDO) {
@@ -44,4 +60,5 @@ if (tarefa.categoria === CategoriaTarefa.ESTUDO) {
 } else if (tarefa.categoria === CategoriaTarefa.LAZER) {
   console.log('Tarefa de lazer');
 }
+
 
