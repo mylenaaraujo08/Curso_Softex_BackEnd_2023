71dc913bcbfb642667d3922ec5497528c5b826e1
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2023-09-09 21:06:37 -0300

    Add files via upload

---
+++ Joyclass/Lógica de programação/Módulo 2/Fila, lista e pilha.js
@@ -0,0 +1,15 @@
+let fila = [3, 7, 9, 1, 0];
+while (fila.length > 0) {
+    fila.shift(); 
+}
+console.log(fila); 
+
+let lista = [3, 7, 9, 1, 0];
+lista = []; 
+console.log(lista); 
+
+let pilha = [3, 7, 9, 1, 0];
+while (pilha.length > 0) {
+    pilha.pop(); 
+}
+console.log(pilha);
