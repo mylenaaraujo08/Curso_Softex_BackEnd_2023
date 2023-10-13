c637ccf2ebc0056298c04577c2f83d90580756f4
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2023-10-13 01:44:36 -0300

    Update calculadoraDeMedia.js

--- JavaScript/calculadoraDeMedia.js
+++ JavaScript/calculadoraDeMedia.js
@@ -5,7 +5,7 @@ var nota3 = parseFloat(prompt("Digite a terceira nota (de 1 a 10):"));
 if (nota1 >= 1 && nota1 <= 10 && nota2 >= 1 && nota2 <= 10 && nota3 >= 1 && nota3 <= 10) {
     var media = (nota1 + nota2 + nota3) / 3;
 
-    console.log("A média das notas é: " + media.toFixed(2)); //toFixed(2) para exibir até duas casas decimais
+    console.log("A média das notas é: " + media.toFixed(2)); 
 } else {
     console.log("Por favor, insira notas entre 1 e 10.");
 }
